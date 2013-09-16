097846ab2c86efbb3dde2337d4d4ccf5d19954a2
soumaya <soumaya@nevada>
2013-09-16 17:53:18 +0200

    modif projet sous-classe

--- src/org/ldv/melun/sio/swingpac/Bidule.java
+++ src/org/ldv/melun/sio/swingpac/Bidule.java
@@ -2,6 +2,8 @@ package org.ldv.melun.sio.swingpac;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,52 +11,76 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 /**
- * Modèle générique d'objet se déplacant dans la fenêtre (la scene)
- * 
- * @date 2013-09-09
- * @author kpu (lycée Léonard de Vinci - Melun - SIO-SLAM)
- * 
- *         TODO: mémoriser le nombre d'impacts d'objet bidules réalisés (pas
- *         subis)
- *         
- *         TODO : reféfinir toString afin de remonter des informations pertinentes 
- * 
- *         TODO (plus difficile) : charger des images dans un tableau et
- *         appliquer la bonne image (dans paintComponent) en fonction de la
- *         direction de déplacement (à la pacman)
- * 
- */
+* Modèle générique d'objet se déplacant dans la fenêtre (la scene)
+*
+* @date 2013-09-09
+* @author soumaya
+*
+* TODO: mémoriser le nombre d'impacts d'objet bidules réalisés (pas
+* subis)
+*
+* TODO : reféfinir toString afin de remonter des informations
+* pertinentes
+*
+* TODO (plus difficile) : charger des images dans un tableau et
+* appliquer la bonne image (dans paintComponent) en fonction de la
+* direction de déplacement (à la pacman)
+*
+*/
+@SuppressWarnings("serial")
 public class Bidule extends JPanel {
 
+	/**
+	 * Charger et afficher une image dans un panel
+	 * @author fobec 2010
+	 */
+	public static class JImagePanel extends JPanel {
+
+	    
+	}
+
+
+	public static final int TAILLE_BIDULE = 0;
+
+  @Override
+  public String toString() {
+    return "Bidule [incY=" + incY + ", incX=" + incX + ", name=" + name
+        + ", DELAY=" + DELAY
+        + ", nbTouches=" + nbTouches + "]";
+  }
+
   /**
-   * Objet reponsable des déclenchement d'appels (voir MoveAction)
-   */
+* Objet reponsable des déclenchement d'appels (voir MoveAction)
+*/
   private Timer timer;
 
   /**
-   * valeur de déplacement en X, Y
-   */
+* valeur de déplacement en X, Y
+*/
   private int incY, incX;
 
   /**
-   * nom de l'instance (TODO : pourrait être pris par défaut via getClass().getName()...)
-   */
+* nom de l'instance (TODO : pourrait être pris par défaut via
+* getClass().getName()...)
+*/
   private String name;
 
   /**
-   * utilisé pour déterminer une valeur 'aléatoire' du DELAI ayant un impact sur
-   * le déplacement
-   */
+* utilisé pour déterminer une valeur 'aléatoire' du DELAI ayant un impact sur
+* le déplacement
+*/
   static private Random alea;
 
   /**
-   * initialisation de la propriété de classe.
-   */
+* initialisation de la propriété de classe.
+*/
   static {
     alea = new Random();
   }
@@ -66,16 +92,24 @@ public class Bidule extends JPanel {
   final int DELAY;
 
   /**
-   * dimension minimale considérant un bidule en vie
-   */
+* dimension minimale considérant un bidule en vie
+*/
   final int NB_MINMAL_PIXELS_VIE = 3;
 
   /**
-   * Déclaration du Template/Hook
-   * 
-   * @author kpu
-   * 
-   */
+* Compte le nombre de fois que this touche un autre bidule,
+* sans être touché lui-même
+*/
+  private int nbTouches;
+
+private boolean selected;
+
+  /**
+* Déclaration du Template/Hook
+*
+* @author soumaya
+*
+*/
   public class MoveAction implements ActionListener {
 
     @Override
@@ -85,20 +119,21 @@ public class Bidule extends JPanel {
       setLocation(getX() + incX, getY() + incY);
       stayOnStage();
       manageCollisions();
-      testWiner();
+      if (Bidule.this.getParent() != null)
+         testWiner();
     }
   }
 
   /**
-   * Constructeur : initialisateur d'instance
-   * 
-   * @param name
-   *          nickname de l'objet
-   */
+* Constructeur : initialisateur d'instance
+*
+* @param name
+* nickname de l'objet
+*/
   public Bidule(String name) {
     super();
     this.name = name;
-    this.setSize(100, 100);
+    this.setSize(50, 50);
     this.setBackground(Color.BLUE);
     this.incX = 1;
     this.incY = 1;
@@ -113,8 +148,8 @@ public class Bidule extends JPanel {
   }
 
   /**
-   * Rester dans l'espace de la scene
-   */
+* Rester dans l'espace de la scene
+*/
   private void stayOnStage() {
     Rectangle rect = getParent().getBounds();
     // System.out.println(rect);
@@ -141,7 +176,7 @@ public class Bidule extends JPanel {
   public void stop() {
     this.timer.stop();
   }
-
+ 
   public int getIncY() {
     return incY;
   }
@@ -165,32 +200,32 @@ public class Bidule extends JPanel {
   }
 
   /**
-   * appelé après un déplacement. Vérifie si impacts, et préviens les objets
-   * touchés.
-   */
+* appelé après un déplacement. Vérifie si impacts, et préviens les objets
+* touchés.
+*/
   private void manageCollisions() {
     // ai-je touché d'autres bidules ?
-    List<Bidule> bidules = getCollisions();
+    List<Bidule> bidules = this.getCollisions();
     for (Bidule bidule : bidules) {
       if (bidule.isGoDown()
           && bidule.getY() + bidule.getHeight() >= this.getY())
-        bidule.tuEstouchePar(this);
+        this.tuEstouchePar(bidule);
       else if (bidule.isGoUp()
           && bidule.getY() <= this.getY() + this.getHeight())
-        bidule.tuEstouchePar(this);
+        this.tuEstouchePar(bidule);
       else if (bidule.isGoRight()
           && bidule.getX() + bidule.getWidth() >= this.getX())
-        bidule.tuEstouchePar(this);
+        this.tuEstouchePar(bidule);
       else if (bidule.isGoLeft()
           && bidule.getX() <= this.getWidth() + this.getX())
-        bidule.tuEstouchePar(this);
+        this.tuEstouchePar(bidule);
     }
   }
 
   /**
-   * Vérifie si l'instance courante gagne la partie. Afffiche un message si
-   * c'est le cas, puis disparait.
-   */
+* Vérifie si l'instance courante gagne la partie. Afffiche un message si
+* c'est le cas, puis disparait.
+*/
   private void testWiner() {
     // le vainqueur est celui qui reste seul
     if (aloneInTheWorld()) {
@@ -201,11 +236,11 @@ public class Bidule extends JPanel {
   }
 
   /**
-   * Détermine si l'objet courant est seul dans la scene
-   * 
-   * @return true si aucun autre objet de type Bidule ne partage la scene avec
-   *         l'objet courant
-   */
+* Détermine si l'objet courant est seul dans la scene
+*
+* @return true si aucun autre objet de type Bidule ne partage la scene avec
+* l'objet courant
+*/
   private boolean aloneInTheWorld() {
     for (Component obj : getParent().getComponents())
       if (obj instanceof Bidule && obj != this)
@@ -214,93 +249,106 @@ public class Bidule extends JPanel {
   }
 
   /**
-   * Appelé par un autre objet lorsqu'il me touche
-   * 
-   * @param biduleImpacteur
-   *          l'objet qui vient de rentrer en collision avec moi
-   */
-  public void tuEstouchePar(Bidule biduleImpacteur) {
+* Appelé par un autre objet lorsqu'il me touche
+*
+* @param biduleQuiATouche
+* l'objet qui vient de rentrer en collision avec moi
+*/
+  public void tuEstouchePar(Bidule biduleQuiATouche) {
+    nbTouches=0;
+    biduleQuiATouche.aTouche();
+    Bidule biduleQuiEstTouche = this;
     // je retrécis
-    this.setBounds(getX() + incX, getY() + incY, getWidth() - 1,
+    biduleQuiEstTouche.setBounds(getX() + incX, getY() + incY, getWidth() - 1,
         getHeight() - 1);
 
     // TODO (plus difficile) : augmenter la taille de biduleImpacteur (dans la
-    // limite de la taille initiale)
-    // si celui-ci a touché au moins 5 autres bidules
+    // limite du quart (un pourcentage) de la taille initiale)
+    // si celui-ci a touché au moins x autres bidules
 
     // en dessous d'une dimension minimale, l'objet
     // courant disparait de ce monde...
-    if (getWidth() < NB_MINMAL_PIXELS_VIE || getHeight() < NB_MINMAL_PIXELS_VIE) {
-      // sucide...
-      this.stop();
-      System.out.println("Je meurs :-(   " + this.name);
-      getParent().remove(this);
-    }
+    if (biduleQuiEstTouche.getWidth() < NB_MINMAL_PIXELS_VIE
+        || biduleQuiEstTouche.getHeight() < NB_MINMAL_PIXELS_VIE) {
+      // suicide...
+     if (biduleQuiEstTouche.getParent() == null) return;
+      biduleQuiEstTouche.stop();
+      System.out.println("Je meurs :-( " + biduleQuiEstTouche.name);
+      biduleQuiEstTouche.getParent().remove(this);
+    } else
+      biduleQuiEstTouche.doAfterImpactByOther();
+  }
 
-    doAfterImpactByOther();
+  private void aTouche() {
+    nbTouches++;
+    if (nbTouches >= 3 && this.getWidth()<10){
+      this.setBounds(getX(), getY(),getWidth()+10, getHeight()+10);
+      nbTouches=0;
+      System.out.println(name + " est augmenté");
+    }
   }
 
   /**
-   * établir une stratégie après impact ; un autre bidule vient de (me) toucher
-   * (toucher l'objet courant)
-   */
+* établir une stratégie après impact ; un autre bidule vient de (me) toucher
+* (toucher l'objet courant)
+*/
   protected void doAfterImpactByOther() {
     // à redéfinir par les classes enfants
   }
 
   /**
-   * obtenir le sens de déplacement
-   * 
-   * @return true if down
-   */
+* obtenir le sens de déplacement
+*
+* @return true if down
+*/
   public boolean isGoDown() {
     return incY > 0;
   }
 
   /**
-   * obtenir le sens de déplacement
-   * 
-   * @return true if up
-   */
+* obtenir le sens de déplacement
+*
+* @return true if up
+*/
   public boolean isGoUp() {
     return incY < 0;
   }
 
   /**
-   * obtenir le sens de déplacement
-   * 
-   * @return true if right
-   */
+* obtenir le sens de déplacement
+*
+* @return true if right
+*/
   public boolean isGoRight() {
     return incX > 0;
   }
 
   /**
-   * obtenir le sens de déplacement
-   * 
-   * @return true if left
-   */
+* obtenir le sens de déplacement
+*
+* @return true if left
+*/
   public boolean isGoLeft() {
     return incX < 0;
   }
 
   /**
-   * Obtenir les objets de la scence en collision avec l'objet courant
-   * 
-   * @return une liste de références aux objets en collision ou une collection
-   *         vide
-   */
+* Obtenir les objets de la scence en collision avec l'objet courant
+*
+* @return une liste de références aux objets en collision ou une collection
+* vide
+*/
   private List<Bidule> getCollisions() {
     return getBidulesProches(0);
   }
 
   /**
-   * Obtenir les objets de la scence à une certaine distance de l'objet courant
-   * 
-   * @param distance
-   *          en pixel
-   * @return une liste de références aux objets proches ou une collection vide
-   */
+* Obtenir les objets de la scène à une certaine distance de l'objet courant
+*
+* @param distance
+* en pixel
+* @return une liste de références aux objets proches ou une collection vide
+*/
   protected List<Bidule> getBidulesProches(int distance) {
     Rectangle mySpace = getBounds();
     // on agrandit le périmetre (en fonction du paramètre distance)
@@ -320,42 +368,42 @@ public class Bidule extends JPanel {
   }
 
   /**
-   * oriente le déplacement vers le bas
-   */
+* oriente le déplacement vers le bas
+*/
   public void goOnDown() {
     if (incY < 0)
       incY *= -1;
   }
 
   /**
-   * oriente le déplacement vers le haut
-   */
+* oriente le déplacement vers le haut
+*/
   public void goOnTop() {
     if (incY > 0)
       incY *= -1;
   }
 
   /**
-   * oriente le déplacement vers la droite
-   */
+* oriente le déplacement vers la droite
+*/
   public void goOnRight() {
     if (incX < 0)
       incX *= -1;
   }
 
   /**
-   * oriente le déplacement vers la gauche
-   */
+* oriente le déplacement vers la gauche
+*/
   public void goOnLeft() {
     if (incX > 0)
       incX *= -1;
   }
 
   /**
-   * appelé par la tache du timer pour déplacer l'objet courant. Suite à cet
-   * appel, l'objet est positionné par setLocation(getX() + incX, getY() + incY);
-   * puis automatiquement recadré dans la scene si nécessaire.
-   */
+* appelé par la tache du timer pour déplacer l'objet courant. Suite à cet
+* appel, l'objet est positionné par setLocation(getX() + incX, getY() +
+* incY); puis automatiquement recadré dans la scene si nécessaire.
+*/
   public void doMove() {
     // obtenir les coordonnées de la scene
     Rectangle rect = getParent().getBounds();
@@ -370,4 +418,32 @@ public class Bidule extends JPanel {
     if (getY() + incY < 0)
       goOnDown();
   }
+
+  
+  public boolean isSelected() {
+	    return selected;
+	  }
+
+	  public void setSelected(boolean selected) {
+	    this.selected = selected;
+	  }
+
+	  @Override
+	  protected void paintComponent(Graphics g) {
+	    super.paintComponent(g);
+	    if (selected) {
+	      Rectangle rect = this.getBounds();
+	      g.setColor(Color.BLACK);
+	      g.drawRect(1, 1, rect.width - 3, rect.height - 3);
+	      g.setColor(Color.LIGHT_GRAY);
+	      g.drawRect(2, 2, rect.width - 4, rect.height - 4);
+	    }
+	  }
+
+
+  public boolean isRunning() {
+    return timer.isRunning();
+  }
+ 
 }
+
